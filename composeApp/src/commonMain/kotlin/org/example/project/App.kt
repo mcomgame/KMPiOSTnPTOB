dac8249508f0056c4a98966f279f79a325b50218
Wittawas <hellomkub@gmail.com>
2024-12-04 15:45:52 +0700

    add project

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -23,6 +23,7 @@ fun App() {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
+
                 Text("Click me!")
             }
             AnimatedVisibility(showContent) {
